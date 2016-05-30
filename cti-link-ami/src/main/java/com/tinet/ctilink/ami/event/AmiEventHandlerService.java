29e2cb39cbd38cc035e2a39b1f60182a47c94df1
Tinet-AaronAn <anjb@ti-net.com.cn>
2016-05-30 16:37:00 +0800

    update

--- cti-link-ami/src/main/java/com/tinet/ctilink/ami/event/AmiEventHandlerService.java
+++ cti-link-ami/src/main/java/com/tinet/ctilink/ami/event/AmiEventHandlerService.java
@@ -38,6 +38,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.tinet.ctilink.ami.inc.AmiChanVarNameConst;
 import com.tinet.ctilink.inc.Const;
 
 /**
@@ -58,12 +59,10 @@ public class AmiEventHandlerService {
 	
 	private Map<Class<?>, AmiUserEventHandler> userEventHandlerMap;
 	private Map<Class<?>, AmiChannelEventHandler> channelEventHandlerMap;
-	private Map<String, String> enterpriseIds;
 	private Map<String, ExecutorService> executors;
 
 	public AmiEventHandlerService() {
 		executors = new HashMap<String, ExecutorService>();
-		enterpriseIds = new HashMap<String, String>();
 	}
 
 	/**
@@ -73,9 +72,9 @@ public class AmiEventHandlerService {
 	 */
 	public void handleUserEvent(ManagerEvent event) {
 
-		String channel = ((UserEvent) event).getChannel();
-		if (StringUtils.isNotEmpty(channel)) {
-			String tail = channel.substring(channel.length() - 1);
+		String mainUniqueId = ((AbstractChannelEvent) event).getChanVarialbe(AmiChanVarNameConst.CDR_MAIN_UNIQUE_ID) ;
+		if (StringUtils.isNotEmpty(mainUniqueId)) {
+			String tail = mainUniqueId.substring(mainUniqueId.length() - 1);
 
 			getExecutor(tail).execute(new Runnable() {
 				@Override
@@ -94,7 +93,7 @@ public class AmiEventHandlerService {
 	 */
 	public void handleChannelEvent(ManagerEvent event, ChannelManager channelManager) {
 
-		String mainUniqueId = ((AbstractChannelEvent) event).getChanVarialbe("cdr_main_unique_id") ;
+		String mainUniqueId = ((AbstractChannelEvent) event).getChanVarialbe(AmiChanVarNameConst.CDR_MAIN_UNIQUE_ID) ;
 		if (StringUtils.isNotEmpty(mainUniqueId)) {
 			String tail = mainUniqueId.substring(mainUniqueId.length() - 1);
 			getExecutor(tail).execute(new Runnable() {
